8f98bdb99fbfe35a97585da48ecf9a388e298dba
Aleksey <aleksey.popryaduhin@yahoo.com>
2018-07-17 20:45:48 +0300

    Компании

--- src/companies/Companies.js
+++ src/companies/Companies.js
@@ -1,13 +1,43 @@
 import React, {Component} from 'react';
-import {Container, Title} from './Companies.styled';
+import {
+    ArrowLeft,
+    ArrowRight,
+    CandyStickLine,
+    CompanyLogo,
+    Container,
+    Dot,
+    Dots,
+    StubCompaniesRow,
+    Title
+} from './Companies.styled';
+import Grid from "@material-ui/core/es/Grid/Grid";
 
 export default class Companies extends Component {
     render() {
         return (
             <Container>
-                <Title>
-                    Дешевые авиабилеты от крупнейших авиакомпаний и агентств
-                </Title>
+                <Grid container direction={"column"} justify={"center"}>
+                    <Title>
+                        Дешевые авиабилеты от крупнейших авиакомпаний и агентств
+                    </Title>
+                    <Grid container direction={"column"} justify={"center"}>
+                        <StubCompaniesRow>
+                            <ArrowLeft src="/img/companies/left-arrow.png" alt="left"/>
+                            <CompanyLogo src="/img/companies/airoflot.png" alt="left"/>
+                            <CompanyLogo src="/img/companies/airlines.png" alt="left"/>
+                            <CompanyLogo src="/img/companies/one-two-trip.png" alt="left"/>
+                            <CompanyLogo src="/img/companies/korean-air.png" alt="left"/>
+                            <CompanyLogo src="/img/companies/close-to-home.png" alt="left"/>
+                            <ArrowRight src="/img/companies/right-arrow.png" alt="left"/>
+                        </StubCompaniesRow>
+                        <Dots>
+                            <Dot src="/img/companies/dot-fill.png" alt=""/>
+                            <Dot src="/img/companies/dot-hollow.png" alt=""/>
+                            <Dot src="/img/companies/dot-hollow.png" alt=""/>
+                        </Dots>
+                    </Grid>
+                </Grid>
+                <CandyStickLine src="/img/companies/candy-stick-line.png" alt=""/>
             </Container>
         )
     }
--- src/companies/Companies.styled.js
+++ src/companies/Companies.styled.js
@@ -1,11 +1,47 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-
+width: 100%;
+margin-top: 60px;
 `;
 
 export const Title = styled.div`
 font-size: 32px;
 line-height: 40px;
 color: #5C5C5C;
+`;
+
+export const StubCompaniesRow = styled.div`
+margin-top: 24px;
+height: 50px;
+display: flex;
+justify-content: center;
+`;
+
+export const ArrowLeft = styled.img`
+height: 32px;
+display: flex;
+align-self: center;
+`;
+
+export const ArrowRight = styled.img`
+height: 32px;
+display: flex;
+align-self: center;
+`;
+
+export const CompanyLogo = styled.img`
+margin: 0 24px;
+`;
+
+export const Dots = styled.div`
+margin-top: 24px;
+`;
+
+export const Dot = styled.img`
+margin: 0 4px;
+`;
+
+export const CandyStickLine = styled.img`
+margin-top: 24px;
 `;
