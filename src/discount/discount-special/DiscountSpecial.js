0f3be4400c9344b970fca7082cf9706c31553847
Aleksey <aleksey.popryaduhin@yahoo.com>
2018-07-19 19:44:18 +0300

    Исправлены абсолютные пути изображений на относительные

--- src/discount/discount-special/DiscountSpecial.js
+++ src/discount/discount-special/DiscountSpecial.js
@@ -11,7 +11,7 @@ export default class DiscountSpecial extends Component {
         {
             id: 1,
             title: 'Билеты от 499 рублей!',
-            companyLogo: '/img/special/pobeda.png',
+            companyLogo: 'img/special/pobeda.png',
             price: 'от 499 ₽',
             fireTime: 'Осталось 45 дней',
             description: 'Билеты от 499 рублей! Специальное предложение от авиакомпании Победа'
@@ -20,7 +20,7 @@ export default class DiscountSpecial extends Component {
             id: 2,
             title: 'В Нью-Йорк от 20680 рублей!',
             titleImg: 'img/special/img.png',
-            companyLogo: '/img/special/lufthansa.png',
+            companyLogo: 'img/special/lufthansa.png',
             price: 'от 20 680 ₽',
             fireTime: 'Осталось 19 дней',
             description: 'Из Москвы в США от 20680 рублей! Специальное предложение от авиакомпании Lufthansa'
@@ -29,7 +29,7 @@ export default class DiscountSpecial extends Component {
             id: 3,
             title: 'В Лос-Анджелес от 22360 р...',
             titleImg: 'img/special/img.png',
-            companyLogo: '/img/special/lufthansa.png',
+            companyLogo: 'img/special/lufthansa.png',
             price: 'от 20 360 ₽',
             fireTime: 'Осталось 19 дней',
             description: 'Из Москвы в США от 22360 рублей! Специальное предложение от авиакомпании Lufthansa'
--- src/flight-directions/cities/Cities.js
+++ src/flight-directions/cities/Cities.js
@@ -9,57 +9,57 @@ export default class Cities extends Component {
     cities = [
         {
             id: 1,
-            countryFlag: '/img/ru.png',
+            countryFlag: 'img/ru.png',
             countryName: 'Россия',
             cityName: 'Краснодар',
             findPriceFrom: 'Найти от 1 212 ₽',
             date: '18 марта',
-            cover: '/img/krasnodar.png'
+            cover: 'img/krasnodar.png'
         },
         {
             id: 2,
-            countryFlag: '/img/ru.png',
+            countryFlag: 'img/ru.png',
             countryName: 'Россия',
             cityName: 'Сочи (Адлер)',
             findPriceFrom: 'Найти от 1 334 ₽',
             date: '27 марта',
-            cover: '/img/sochi.png'
+            cover: 'img/sochi.png'
         },
         {
             id: 3,
-            countryFlag: '/img/ru.png',
+            countryFlag: 'img/ru.png',
             countryName: 'Россия',
             cityName: 'Санкт-Петербург',
             findPriceFrom: 'Найти от 1 508 ₽',
             date: '19 февраля',
-            cover: '/img/spb.png'
+            cover: 'img/spb.png'
         },
         {
             id: 4,
-            countryFlag: '/img/ru.png',
+            countryFlag: 'img/ru.png',
             countryName: 'Россия',
             cityName: 'Минеральные воды',
             findPriceFrom: 'Найти от 2 074 ₽',
             date: '13 марта',
-            cover: '/img/aquaminerale.png'
+            cover: 'img/aquaminerale.png'
         },
         {
             id: 5,
-            countryFlag: '/img/ru.png',
+            countryFlag: 'img/ru.png',
             countryName: 'Россия',
             cityName: 'Симферополь (Крым)',
             findPriceFrom: 'Найти от 2 407 ₽',
             date: '13 марта',
-            cover: '/img/simpheropol.png'
+            cover: 'img/simpheropol.png'
         },
         {
             id: 6,
-            countryFlag: '/img/ru.png',
+            countryFlag: 'img/ru.png',
             countryName: 'Испания',
             cityName: 'Барселона',
             findPriceFrom: 'Найти от 4 247 ₽',
             date: '24 марта',
-            cover: '/img/barselona.png'
+            cover: 'img/barselona.png'
         },
     ];
 
