1369916b57b288842379d05892a257510373da5b
Aleksey <aleksey.popryaduhin@yahoo.com>
2018-07-23 12:39:03 +0300

    basedir

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const target = document.querySelector('#root');
 
 render(
     <Provider store={store}>
-        <ConnectedRouter history={history}>
+        <ConnectedRouter history={history} basename="/aviasales-demo-frontend">
             <div>
                 <App/>
             </div>
